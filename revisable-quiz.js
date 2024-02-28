6b6274fe5f625bb7a9daa7e814e70d60e49ece08
Remi Duval <contact@remiduval.com>
2024-02-28 17:47:27 +0000

    initial commit

---
+++ revisable-quiz.js
@@ -0,0 +1,24 @@
+<script>
+(function() {
+  'use strict';
+
+  require.config({
+    paths: {
+      CerosSDK: "//sdk.ceros.com/standalone-player-sdk-v5.min",
+    },
+  });
+
+  require(['CerosSDK'], function(CerosSDK) {
+    CerosSDK.findExperience()
+        .fail(function(error) {
+            console.error("Error finding Ceros experience:", error);
+        })
+        .done(function(experience) {
+          console.log("hi");
+console.log(experience.findLayersByTag("answer"));
+const layers = experience.findLayersByTag("answer");
+          console.log(`Found ${layers.length} answer layers`);
+        });
+  });
+})();
+</script>
